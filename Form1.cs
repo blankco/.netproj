60f5ff30f9fb7c586dc84ee6995e715ea7e18014
blankco <56691302+blankco@users.noreply.github.com>
2019-10-20 14:44:56 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        bool choice = true;
+        string[] prizes = { "a live goat!!", "a big-screen!", "a Car!" };
+        int randNum;
+        Random ranNumberGenerator = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            randNum = ranNumberGenerator.Next(0, 3);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //the random number is assigned to this button
+            if (choice)
+            {
+                //randomly assign prizes
+                button1.Tag = randNum;
+                if (randNum == 0)
+                {
+                    button2.Tag = 1;
+                    button3.Tag = 2;
+                }
+                else if (randNum == 1)
+                {
+                    button2.Tag = 0;
+                    button3.Tag = 2;
+                }
+                else
+                {
+                    button2.Tag = 0;
+                    button3.Tag = 1;
+                }
+
+                if (Convert.ToInt32(button2.Tag) < Convert.ToInt32(button3.Tag))
+                    textBox1.Text = "Door 2 has " + prizes[Convert.ToInt32(button2.Tag)] + ". You can change doors now!";
+                else
+                    textBox1.Text = "Door 3 has " + prizes[Convert.ToInt32(button3.Tag)] + ". You can change doors now!";
+                choice = false;
+            }
+            else
+            {
+                textBox1.Text = "Congratulations you've won " + prizes[Convert.ToInt32(button1.Tag)];
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (choice)
+            {
+                //randomly assign prizes
+                button2.Tag = randNum;
+                if (randNum == 0)
+                {
+                    button1.Tag = 1;
+                    button3.Tag = 2;
+                }
+                else if (randNum == 1)
+                {
+                    button1.Tag = 0;
+                    button3.Tag = 2;
+                }
+                else
+                {
+                    button1.Tag = 0;
+                    button3.Tag = 1;
+                }
+
+                if (Convert.ToInt32(button1.Tag) < Convert.ToInt32(button3.Tag))
+                    textBox1.Text = "Door 1 has " + prizes[Convert.ToInt32(button1.Tag)] + ". You can change doors now!";
+                else
+                    textBox1.Text = "Door 3 has " + prizes[Convert.ToInt32(button3.Tag)] + ". You can change doors now!";
+                choice = false;
+            }
+            else
+            {
+                textBox1.Text = "Congratulations you've won " + prizes[Convert.ToInt32(button2.Tag)];
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+            }
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (choice)
+            {
+                //randomly assign prizes
+                button2.Tag = randNum;
+                if (randNum == 0)
+                {
+                    button1.Tag = 1;
+                    button3.Tag = 2;
+                }
+                else if (randNum == 1)
+                {
+                    button1.Tag = 0;
+                    button3.Tag = 2;
+                }
+                else
+                {
+                    button1.Tag = 0;
+                    button3.Tag = 1;
+                }
+
+                if (Convert.ToInt32(button1.Tag) < Convert.ToInt32(button3.Tag))
+                    textBox1.Text = "Door 1 has " + prizes[Convert.ToInt32(button1.Tag)] + ". You can change doors now!";
+                else
+                    textBox1.Text = "Door 3 has " + prizes[Convert.ToInt32(button3.Tag)] + ". You can change doors now!";
+                choice = false;
+            }
+            else
+            {
+                textBox1.Text = "Congratulations you've won " + prizes[Convert.ToInt32(button2.Tag)];
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (choice)
+            {
+                //randomly assign prizes
+                button3.Tag = randNum;
+                if (randNum == 0)
+                {
+                    button1.Tag = 1;
+                    button2.Tag = 2;
+                }
+                else if (randNum == 1)
+                {
+                    button1.Tag = 0;
+                    button2.Tag = 2;
+                }
+                else
+                {
+                    button1.Tag = 0;
+                    button2.Tag = 1;
+                }
+                if (Convert.ToInt32(button1.Tag) < Convert.ToInt32(button2.Tag))
+                    textBox1.Text = "Door 1 has " + prizes[Convert.ToInt32(button1.Tag)] + ". You can change doors now!";
+                else
+                    textBox1.Text = "Door 2 has " + prizes[Convert.ToInt32(button2.Tag)] + ". You can change doors now!";
+                choice = false;
+            }
+            else
+            {
+                textBox1.Text = "Congratulations you've won " + prizes[Convert.ToInt32(button3.Tag)];
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+            }
+        }
+
+        
+    }
+}
